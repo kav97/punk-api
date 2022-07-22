cc1481b73f405f3c65c3e54f90808a7d43a3f028
Kavandeep <kavan1@live.co.uk>
2022-07-22 17:50:16 +0100

    minor changes to app styling

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,7 @@ const App = () => {
   return (
     <>
       <div className="app">
-        <main>
-          <CardList beers={beers} />
-        </main>
+        <CardList beers={beers} />
       </div>
     </>
   );
