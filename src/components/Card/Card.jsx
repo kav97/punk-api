87f76adb87f0f57454b31983a000378dfccd3880
Kavandeep <kavan1@live.co.uk>
2022-07-22 17:49:31 +0100

    state for card flip + card styling

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,16 +1,35 @@
 import "./Card.scss";
+import { useState } from "react";
 
-const Card = ({name, img, abv, description }) => {
-    return (
-        <div className="card">
-            <img className="card__img" src={img} alt={name} />
-            <div className="card-content">
-                <h2 className="card-content__name">{name}</h2>
-                <p className="card-content__abv">ABV:{abv}%</p>
-                <p className="card-content__description">{description}</p>
-            </div>
-        </div>
-    )
-}
+const Card = ({ name, img, abv, ph, year, description }) => {
+  const [showInfo, setShowInfo] = useState(false);
+  const handleClick = () => {
+    setShowInfo(!showInfo);
+  };
+
+  const cardFrontJSX = (
+    <>
+      <img className="card__img" src={img} alt={name} />
+      <h2 className="card__name">{name}</h2>
+      <div className="card-content">
+        <p className="card-content__year"><b>First Brewed:</b> {year}</p>
+        <p className="card-content__abv"><b>ABV:</b> {abv}%</p>
+        <p className="card-content__ph"><b>pH:</b> {ph}</p>
+      </div>
+    </>
+  );
+
+  const cardBackJSX = (
+    <div className="card-back">
+      <p className="card-back__description">{description}</p>
+    </div>
+  );
+
+  return (
+    <div className="card" onClick={handleClick}>
+      {showInfo ? cardBackJSX : cardFrontJSX}
+    </div>
+  );
+};
 
 export default Card;
--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -2,13 +2,19 @@ import "./CardList.scss";
 import Card from "../Card/Card";
 
 const CardList = ({ beers }) => {
-    const cardListJSX = beers.map(beer => <Card key={beer.id} name={beer.name} img={beer.image_url} abv={beer.abv} description={beer.description} />)
+  const cardListJSX = beers.map((beer) => (
+    <Card
+      key={beer.id}
+      name={beer.name}
+      img={beer.image_url}
+      abv={beer.abv}
+      ph={beer.ph}
+      year={beer.first_brewed}
+      description={beer.description}
+    />
+  ));
 
-    return (
-        <div className="card-list">
-            {cardListJSX}
-        </div>
-    )
-}
+  return <div className="card-list">{cardListJSX}</div>;
+};
 
 export default CardList;
