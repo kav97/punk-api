1fedcbe442faf1951e72abec493157c2dac1dd51
Kavandeep <kavan1@live.co.uk>
2022-07-29 12:28:13 +0100

    looped all data and loaded on single page

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,29 @@
-import './App.scss';
-//import beers from './data/beers';
-import CardList from './components/CardList/CardList'
-import SearchBox from './components/SearchBox/SearchBox';
+import "./App.scss";
+import CardList from "./components/CardList/CardList";
+import SearchBox from "./components/SearchBox/SearchBox";
+//import RadioButtons from "./components/RadioButtons/RadioButtons";
 import { useState, useEffect } from "react";
 
+//import Main from "./containers/Main/Main"
 
-const App = () => { 
-
+const App = () => {
   const [beers, setBeers] = useState([]);
+  // const [filter, setFilter] = useState("all");
 
   const getBeers = async () => {
-    const response = await fetch("https://api.punkapi.com/v2/beers");
-    const data = await response.json();
+    const arr = [];
+    for (let i = 1; i < 6; i++) {
+      const url = `https://api.punkapi.com/v2/beers?page=${i}&per_page=80`;
+      const response = await fetch(url);
+      const data = await response.json();
+    
+      Array.prototype.push.apply(arr, data);
+    }
+    setBeers(arr);
 
-    setBeers(data);
   };
 
-  useEffect(() => {
-    getBeers();
-  }, [])
-  
-  //console.log(beers);
-
   //search by entering letters into searchbox
-  
   const [searchTerm, setSearchTerm] = useState("");
 
   const handleInput = (event) => {
@@ -31,19 +31,30 @@ const App = () => {
     setSearchTerm(cleanInput);
   };
 
-  const filteredBeers = beers.filter(beer => {
+  const filteredBeers = beers.filter((beer) => {
     const beerLower = beer.name.toLowerCase();
     return beerLower.includes(searchTerm);
-  })
+  });
+
+
+  useEffect(() => {
+    getBeers();
+  }, []);
 
   return (
     <>
       <div className="app">
-        <SearchBox handleInput={handleInput} searchTerm={searchTerm}/>
+        <SearchBox handleInput={handleInput} searchTerm={searchTerm} />
+        {/* <RadioButtons
+          label="Filter by:"
+          options={["All", "ABV (>6.0%)", "Range (Brewed before 2010)", "Acidic pH (< 4)"]}
+          onChange={handleRadioChange}
+          selected={filter}
+        /> */}
         <CardList beers={filteredBeers} />
       </div>
     </>
   );
-}
+};
 
 export default App;
---
+++ src/containers/Main/Main.jsx
@@ -0,0 +1,10 @@
+import "./Main.scss"
+// import CardList from "../../components/CardList/CardList";
+
+// const Main = ({beers}) => {
+//     return (
+//         <CardList beers={filteredBeers} />
+//     )
+// }
+
+// export default Main
