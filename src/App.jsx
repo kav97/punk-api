2e591c4252fad874a04cfb6015dcacaa48ea6dd0
Kavandeep <kavan1@live.co.uk>
2022-09-20 12:27:13 +0100

    line added to avoid linting error in useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -34,6 +34,7 @@ const App = () => {
 
   useEffect(() => {
     getBeers();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [abv, brewed, ph])
 
   const handleInput = (event) => {
